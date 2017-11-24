cd4c7ce2367a8a61630ec2dd26c9ec081027071d
Jaime Vendrame <jaimevendrame@MacBook-Pro-de-Jaime.local>
2017-11-24 18:08:05 -0200

    pronac4 4

--- app/Http/Controllers/Painel/CampanhaController.php
+++ app/Http/Controllers/Painel/CampanhaController.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\Painel;
 
-use App\Http\Controllers\StandardController;
+namespace App\Http\Controllers;
+
+
 use App\Models\Painel\Campanha;
-use App\Models\Painel\Role;
 use Illuminate\Http\Request;
 
 class CampanhaController extends StandardController
@@ -13,14 +13,13 @@ class CampanhaController extends StandardController
     protected $nameView = 'painel.campanhas';
     protected $redirectCad = '/admin/campanhas/cadastrar';
     protected $redirectEdit = '/admin/campanhas/editar';
-    protected $route = '/admin/campanhas';
-    protected $brand = ['Campanha','Cadastro'];
+    protected $route = '/admin/pacote';
 
 
 
-    public function __construct(Campanha $model, Request $request)
+    public function __construct(Campanha $campanha, Request $request)
     {
-        $this->model = $model;
+        $this->model = $campanha;
         $this->request = $request;
     }
 }
