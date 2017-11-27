aec64a4eacd95c79b63d7e973be49d8793471c78
Jaime Vendrame <jaimevendrame@MacBook-Pro-de-Jaime.local>
2017-11-26 22:22:48 -0200

    pronac4 digitalocean 4

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <title>PRONAP</title>
+    <title>PRONAC</title>
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
     <!--CSS Personalizado-->
     <link rel="stylesheet" href="{{url('assets/css/pronap.css')}}">
